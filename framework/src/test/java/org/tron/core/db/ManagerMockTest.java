6f1f8a7bc31191f48a8e522245c6a9819594368c
chaozhu <chaosuper85@gmail.com>
2024-09-13 08:32:19 +0800

    optimize tests stable

--- framework/src/test/java/org/tron/core/db/ManagerMockTest.java
+++ framework/src/test/java/org/tron/core/db/ManagerMockTest.java
@@ -1,14 +1,20 @@
 package org.tron.core.db;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import com.google.protobuf.Any;
 import com.google.protobuf.ByteString;
 import java.nio.charset.StandardCharsets;
+
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.After;
 import org.junit.Before;
@@ -23,6 +29,8 @@ import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.powermock.reflect.Whitebox;
+import org.quartz.CronExpression;
+import org.tron.common.parameter.CommonParameter;
 import org.tron.common.runtime.ProgramResult;
 import org.tron.common.runtime.RuntimeImpl;
 import org.tron.common.utils.Sha256Hash;
@@ -31,6 +39,16 @@ import org.tron.core.capsule.BlockCapsule;
 import org.tron.core.capsule.TransactionCapsule;
 import org.tron.core.capsule.TransactionInfoCapsule;
 import org.tron.core.capsule.utils.TransactionUtil;
+import org.tron.core.config.args.Args;
+import org.tron.core.exception.ContractSizeNotEqualToOneException;
+import org.tron.core.exception.DupTransactionException;
+import org.tron.core.exception.ItemNotFoundException;
+import org.tron.core.exception.ReceiptCheckErrException;
+import org.tron.core.exception.TaposException;
+import org.tron.core.exception.TooBigTransactionException;
+import org.tron.core.exception.TooBigTransactionResultException;
+import org.tron.core.exception.TransactionExpirationException;
+import org.tron.core.exception.ValidateSignatureException;
 import org.tron.core.store.AccountStore;
 import org.tron.core.store.BalanceTraceStore;
 import org.tron.core.store.DynamicPropertiesStore;
@@ -38,7 +56,10 @@ import org.tron.protos.Protocol;
 import org.tron.protos.contract.BalanceContract;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({Manager.class, TransactionUtil.class})
+@PrepareForTest({Manager.class
+    , CommonParameter.class
+    , Args.class
+    , TransactionUtil.class})
 @Slf4j
 public class ManagerMockTest {
   @Spy
@@ -147,9 +168,203 @@ public class ManagerMockTest {
     assertNotNull(dbManager.processTransaction(trxCapMock, blockCapMock));
   }
 
+  private void initMockEnv(long headNum, long headTime
+      , long exitHeight
+      , long exitCount
+      , String blockTime) throws Exception {
+    ChainBaseManager chainBaseManagerMock = PowerMockito.mock(ChainBaseManager.class);
+    Args argsMock = PowerMockito.mock(Args.class);
 
-  
+    PowerMockito.mockStatic(CommonParameter.class);
+    PowerMockito.mockStatic(Args.class);
+    PowerMockito.when(Args.getInstance()).thenReturn(argsMock);
 
+    when(chainBaseManagerMock.getHeadBlockNum()).thenReturn(headNum);
+    when(chainBaseManagerMock.getHeadBlockTimeStamp()).thenReturn(headTime);
 
+    when(argsMock.getShutdownBlockHeight()).thenReturn(exitHeight);
+    when(argsMock.getShutdownBlockCount()).thenReturn(exitCount);
+    when(argsMock.isP2pDisable()).thenReturn(false);
+    when(argsMock.getShutdownBlockTime()).thenReturn(new CronExpression(blockTime));  //"0 0 12 * * ?"
+
+    Whitebox.setInternalState(dbManager, "chainBaseManager", chainBaseManagerMock);
+  }
+
+  @Test
+  public void testInitAutoStop() throws Exception {
+    initMockEnv(100L, 12345L
+        , 10L, 0L, "0 0 12 * * ?");
+    try {
+      Whitebox.invokeMethod(dbManager, "initAutoStop");
+    } catch (Exception e) {
+      assertEquals("shutDownBlockHeight 10 is less than headNum 100", e.getMessage());
+    }
+  }
+
+  @Test
+  public void testInitAutoStop1() throws Exception {
+    initMockEnv(10L, 12345L
+        , 100L, 0L, "0 0 12 * * ?");
+    try {
+      Whitebox.invokeMethod(dbManager, "initAutoStop");
+    } catch (Exception e) {
+      assertEquals("shutDownBlockCount 0 is less than 1", e.getMessage());
+    }
+  }
+
+  @Test
+  public void testInitAutoStop2() throws Exception {
+    initMockEnv(10L, 99726143865000L
+        , 100L, 1L, "0 0 12 * * ?");
+    try {
+      Whitebox.invokeMethod(dbManager, "initAutoStop");
+    } catch (Exception e) {
+      assertEquals("shutDownBlockTime 0 0 12 * * ? is illegal", e.getMessage());
+    }
+  }
+
+  @Test
+  public void testInitAutoStop3() throws Exception {
+    initMockEnv(10L, 12345L
+        , 100L, 1L, "0 0 12 * * ?");
+    try {
+      Whitebox.invokeMethod(dbManager, "initAutoStop");
+    } catch (Exception e) {
+      assertEquals("shutDownBlockHeight 100 and shutDownBlockCount 1 set both", e.getMessage());
+    }
+  }
+
+  @Test
+  public void testInitAutoStop4() throws Exception {
+    initMockEnv(10L, 12345L
+        , 100L, -1L, "0 0 12 * * ?");
+    try {
+      Whitebox.invokeMethod(dbManager, "initAutoStop");
+    } catch (Exception e) {
+      assertEquals("shutDownBlockHeight 100 and shutDownBlockTime 0 0 12 * * ? set both", e.getMessage());
+    }
+  }
+
+  @Test
+  public void testInitAutoStop5() throws Exception {
+    initMockEnv(10L, 12345L
+        , 0L, 1L, "0 0 12 * * ?");
+    try {
+      Whitebox.invokeMethod(dbManager, "initAutoStop");
+    } catch (Exception e) {
+      assertEquals("shutDownBlockCount 1 and shutDownBlockTime 0 0 12 * * ? set both", e.getMessage());
+    }
+  }
+
+  @Test
+  public void testInitAutoStop6() throws Exception {
+    initMockEnv(-10L, 12345L
+        , -10L, -1L, "0 0 12 * * ?");
+    Whitebox.invokeMethod(dbManager, "initAutoStop");
+    assertEquals(true, true);
+  }
+
+  @Test
+  public void testProcessTransaction() throws Exception {
+    TransactionCapsule transactionCapsuleMock = null;
+    BlockCapsule blockCapsuleMock = PowerMockito.mock(BlockCapsule.class);
+    Whitebox.invokeMethod(dbManager, "processTransaction", transactionCapsuleMock, blockCapsuleMock);
+    assertEquals(true, true);
+  }
+
+  @Test
+  public void testProcessTransaction1() throws Exception {
+    Protocol.Transaction transaction = Protocol.Transaction.newBuilder().setRawData(
+        Protocol.Transaction.raw.newBuilder()
+            .setData(ByteString.copyFrom("sb.toString()".getBytes(StandardCharsets.UTF_8))))
+        .build();
+    TransactionCapsule trxCap = new TransactionCapsule(transaction);
+
+    BlockCapsule blockCapsuleMock = PowerMockito.mock(BlockCapsule.class);
+    try {
+      Whitebox.invokeMethod(dbManager, "processTransaction", trxCap, blockCapsuleMock);
+    } catch (Exception e) {
+      assertTrue(e instanceof ContractSizeNotEqualToOneException);
+    }
+  }
+
+  @SneakyThrows
+  @Test
+  public void testRePush() {
+    Protocol.Transaction transaction = Protocol.Transaction.newBuilder().build();
+    TransactionCapsule trx = new TransactionCapsule(transaction);
+    TransactionStore transactionStoreMock = mock(TransactionStore.class);
+
+    ChainBaseManager chainBaseManagerMock = PowerMockito.mock(ChainBaseManager.class);
+    Whitebox.setInternalState(dbManager, "chainBaseManager", chainBaseManagerMock);
+    when(chainBaseManagerMock.getTransactionStore()).thenReturn(transactionStoreMock);
+    when(transactionStoreMock.has(any())).thenReturn(true);
+
+    dbManager.rePush(trx);
+    assertEquals(true, true);
+  }
+
+  @SneakyThrows
+  @Test
+  public void testRePush1() {
+    Protocol.Transaction transaction = Protocol.Transaction.newBuilder().build();
+    TransactionCapsule trx = new TransactionCapsule(transaction);
+    TransactionStore transactionStoreMock = mock(TransactionStore.class);
+
+    ChainBaseManager chainBaseManagerMock = PowerMockito.mock(ChainBaseManager.class);
+    Whitebox.setInternalState(dbManager, "chainBaseManager", chainBaseManagerMock);
+    when(chainBaseManagerMock.getTransactionStore()).thenReturn(transactionStoreMock);
+    when(transactionStoreMock.has(any())).thenReturn(false);
+
+    doThrow(new ValidateSignatureException()).when(dbManager).pushTransaction(any());
+    dbManager.rePush(trx);
+
+    doThrow(new DupTransactionException()).when(dbManager).pushTransaction(any());
+    dbManager.rePush(trx);
+
+    doThrow(new TaposException()).when(dbManager).pushTransaction(any());
+    dbManager.rePush(trx);
+
+    doThrow(new TooBigTransactionException()).when(dbManager).pushTransaction(any());
+    dbManager.rePush(trx);
+
+    doThrow(new TransactionExpirationException()).when(dbManager).pushTransaction(any());
+    dbManager.rePush(trx);
+
+    doThrow(new ReceiptCheckErrException()).when(dbManager).pushTransaction(any());
+    dbManager.rePush(trx);
+
+    doThrow(new TooBigTransactionResultException()).when(dbManager).pushTransaction(any());
+    dbManager.rePush(trx);
+    assertEquals(true, true);
+  }
+
+  @Test
+  public void testPostSolidityFilter() throws Exception {
+    Whitebox.invokeMethod(dbManager, "postSolidityFilter", 100L, 10L);
+    assertEquals(true, true);
+  }
+
+  @Test
+  public void testReOrgLogsFilter() throws Exception {
+    CommonParameter commonParameterMock = PowerMockito.mock(Args.class);
+    PowerMockito.mockStatic(CommonParameter.class);
+    ChainBaseManager chainBaseManagerMock = PowerMockito.mock(ChainBaseManager.class);
+
+    PowerMockito.when(CommonParameter.getInstance()).thenReturn(commonParameterMock);
+    when(commonParameterMock.isJsonRpcHttpFullNodeEnable()).thenReturn(true);
+    when(chainBaseManagerMock.getDynamicPropertiesStore())
+        .thenReturn(mock(DynamicPropertiesStore.class));
+    Whitebox.setInternalState(dbManager, "chainBaseManager", chainBaseManagerMock);
+    doThrow(new ItemNotFoundException()).when(chainBaseManagerMock).getBlockById(any());
+
+    Whitebox.invokeMethod(dbManager, "reOrgLogsFilter");
+    assertEquals(true, true);
+  }
+
+  @Test
+  public void testGetContinuousBlockCapsule() {
+    
+  }
 
 }
